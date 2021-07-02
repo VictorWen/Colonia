50ef287e1fb969c46b2a41119b4280319884e89d
Victor Wen <vicwen02@g.ucla.edu>
2021-07-02 15:01:13 -0400

    Refactored ConstructedTileGhost

--- Assets/Scripts/Cities/Construction/Projects/ConstructedTileGhost.cs
+++ Assets/Scripts/Cities/Construction/Projects/ConstructedTileGhost.cs
@@ -12,18 +12,6 @@ namespace Cities.Construction.Projects
         public Canvas canvas;
         public Text tooltipText;
 
-        public Coroutine Place(City city, World world, ConstructedTileProject project, GUIStateManager state)
-        {
-            gameObject.SetActive(true);
-            return StartCoroutine(WaitForPlacement(city, world, project, state));
-        }
-
-        public void Place2(City city, World world, ConstructedTileProject project, GUIStateManager state)
-        {
-            gameObject.SetActive(true);
-            StartCoroutine(WaitForPlacement(city, world, project, state));
-        }
-
         private City city;
         private World world;
         private ConstructedTileProject project;
@@ -46,58 +34,94 @@ namespace Cities.Construction.Projects
         public IEnumerator WaitForPlacement(City city, World world, ConstructedTileProject project, GUIStateManager state)
         {
             state.SetState(GUIStateManager.GHOST_TILE);
+
+            GrabProjectSprite();
+
+            HashSet<Vector3Int> range = city.GetCityRange(world);
+            HighlightValidTiles(range);
+
+            yield return new WaitForSeconds(0.1f);
+
+            yield return WaitForValidClick();
+
+            ClearHighlightedTiles(range);
+
+            Debug.Log("Tile Selected");
+            state.SetState(GUIStateManager.CITY);
+            Destroy(gameObject);
+            yield break;
+        }
+
+        private IEnumerator WaitForValidClick()
+        {
+            Vector3Int gridPos = world.WorldToCell(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+            bool click = Input.GetMouseButtonUp(0);
+            while (!click || !IsValidTile(gridPos, city, world, project))
+            {
+                gridPos = ClickOnTile();
+
+                click = Input.GetMouseButtonUp(0);
+                yield return null;
+            }
+
+            PlaceConstructedTile(gridPos);
+
+            yield break;
+        }
+
+        private void GrabProjectSprite()
+        {
             char sepChar = System.IO.Path.DirectorySeparatorChar;
             spriteRenderer.sprite = Resources.Load<Sprite>("Projects" + sepChar + "Constructed Tiles" + sepChar + project.ID);
             spriteRenderer.color = new Color(0.5f, 0.5f, 0.5f);
+        }
 
-            Vector3Int gridPos = world.WorldToCell(Camera.main.ScreenToWorldPoint(Input.mousePosition));
-
-            HashSet<Vector3Int> range = city.GetCityRange(world);
+        private void HighlightValidTiles(HashSet<Vector3Int> range)
+        {
             UnityEngine.Tilemaps.Tile green = Resources.Load<UnityEngine.Tilemaps.Tile>(System.IO.Path.Combine("Tiles", "Green"));
             foreach (Vector3Int pos in range)
             {
                 if (IsValidTile(pos, city, world, project))
                     world.SetMovementTile(pos, green);
             }
+        }
 
-            yield return new WaitForSeconds(0.1f);
+        private Vector3Int ClickOnTile()
+        {
+            Vector3Int gridPos = world.WorldToCell(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+            transform.position = world.CellToWorld(gridPos);
 
-            bool click = Input.GetMouseButtonUp(0);
-            while (!click || !IsValidTile(gridPos, city, world, project))
+            SetToolTipText(gridPos);
+
+            SetSpriteColor(gridPos);
+
+            return gridPos;
+        }
+
+        private void SetToolTipText(Vector3Int gridPos)
+        {
+            string text = project.GetTooltipText(gridPos, world);
+            if (text != null)
             {
-                gridPos = world.WorldToCell(Camera.main.ScreenToWorldPoint(Input.mousePosition));
-                transform.position = world.CellToWorld(gridPos);
-
-                string text = project.GetTooltipText(gridPos, world);
-                if (text != null)
-                {
-                    if (!canvas.gameObject.activeSelf)
-                    {
-                        canvas.gameObject.SetActive(true);
-                    }
-                    tooltipText.text = text;
-                }
-                else
-                {
-                    if (canvas.gameObject.activeSelf)
-                    {
-                        canvas.gameObject.SetActive(false);
-                    }
-                }
-
-                if (!IsValidTile(gridPos, city, world, project))
-                {
-                    spriteRenderer.color = new Color(0, 0, 0);
-                }
-                else
-                {
-                    spriteRenderer.color = new Color(0.5f, 0.5f, 0.5f);
-                }
-
-                click = Input.GetMouseButtonUp(0);
-                yield return null;
+                if (!canvas.gameObject.activeSelf)
+                    canvas.gameObject.SetActive(true);
+                tooltipText.text = text;
             }
+            else if (canvas.gameObject.activeSelf)
+                canvas.gameObject.SetActive(false);
+        }
 
+        private void SetSpriteColor(Vector3Int gridPos)
+        {
+            if (!IsValidTile(gridPos, city, world, project))
+                spriteRenderer.color = new Color(0, 0, 0);
+            else
+                spriteRenderer.color = new Color(0.5f, 0.5f, 0.5f);
+        }
+
+        private void PlaceConstructedTile(Vector3Int gridPos)
+        {
+            char sepChar = System.IO.Path.DirectorySeparatorChar;
             ConstructedTile tile = Resources.Load<ConstructedTile>("Projects" + sepChar + "Constructed Tiles" + sepChar + project.ID);
             if (world.GetConstructedTile(gridPos) != null)
                 project.OnPlacement(gridPos, world.GetConstructedTile(gridPos).Project);
@@ -105,16 +129,12 @@ namespace Cities.Construction.Projects
                 project.OnPlacement(gridPos);
 
             world.StartConstructionOfCityTile(tile, gridPos);
+        }
 
+        private void ClearHighlightedTiles(HashSet<Vector3Int> range)
+        {
             foreach (Vector3Int pos in range)
-            {
                 world.SetMovementTile(pos, null);
-            }
-
-            Debug.Log("Tile Selected");
-            state.SetState(GUIStateManager.CITY);
-            Destroy(gameObject);
-            yield break;
         }
 
         private bool IsValidTile(Vector3Int pos, City city, World world, ConstructedTileProject project)
